b1ef2f81b85485554121580de39ca42e117729be
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-05-24 09:47:32 +0800

    修改rabbitmq重连机制

--- intrusion_customer.py
+++ intrusion_customer.py
@@ -15,20 +15,20 @@ from conf.config_function import detection_timer, logging_handle
 
 
 class DetectionHandler(object):
-    def __init__(self, username, password, host, queue_name, logger_intrusion, init_image):
+    def __init__(self, username, password, host, queue_name, logger_handle, init_image):
         '''
-        安全着装handler定义
+        handler定义
         :param username: rabbitmq name
         :param password: rabbitmq password
         :param host: rabbitmq ip
         :param queue_name: rabbitmq queue name
-        :param logger_intrusion: logging
+        :param logger_handle: logging
         '''
         self.username = username
         self.password = password
         self.host = host
         self.queue_name = queue_name
-        self.logger_intrusion = logger_intrusion
+        self.logger_handle = logger_handle
         # Init intrusion function
         self.fn = Detection_YOLOV3(MODE_PATH_DICT["intrusion"]["model_weights"],
                                    MODE_PATH_DICT["intrusion"]["model_label"],
@@ -38,38 +38,45 @@ class DetectionHandler(object):
         self.channel, self.connection = self.load_channel()
 
     def load_channel(self):
-        try:
-            user_pwd = pika.PlainCredentials(self.username, self.password)
-            # 创建连接对象
-            connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host, credentials=user_pwd))
-            # 创建频道对象
-            channel = connection.channel()
-            # 切换到指定的队列,如果不存在则创建
-            channel.queue_declare(queue=self.queue_name, durable=True)
-            return channel, connection
-        except Exception as e:
-            logger_intrusion.info(f'[INFO] intrusion load_channel error.')
-            return False
-
-    def keep_alive(self):
-        # on time seed heartbeat
-        self.connection.process_data_events()
+        is_connection = True
+        while is_connection:
+            try:
+                user_pwd = pika.PlainCredentials(self.username, self.password)
+                # 创建连接对象
+                connection = pika.BlockingConnection(
+                    pika.ConnectionParameters(host=self.host, credentials=user_pwd, heartbeat_interval=0))
+                # 创建频道对象
+                channel = connection.channel()
+                # 切换到指定的队列,如果不存在则创建
+                channel.queue_declare(queue=self.queue_name, durable=True)
+                channel.basic_consume(
+                    queue=self.queue_name,
+                    # 回调函数
+                    consumer_callback=self.callback,
+                    no_ack=True
+                )
+                is_connection = False
+                return channel, connection
+            except Exception as e:
+                time.sleep(5)
 
     def detection(self):
         """消费，接收消息..."""
-        self.channel.basic_consume(
-            queue=self.queue_name,
-            # 回调函数
-            consumer_callback=self.callback,
-            no_ack=True
-        )
-        self.channel.start_consuming()
+        disconnected = True
+        while disconnected:
+            try:
+                disconnected = False
+                self.channel.start_consuming()  # blocking call
+            except pika.exceptions.ConnectionClosed:  # when connection is lost, e.g. rabbitmq not running
+                self.logger_handle.error("Lost connection to rabbitmq service on manager")
+                disconnected = True
+                time.sleep(5)  # reconnect timer
+                self.logger_handle.info("Trying to reconnect...")
+                self.channel, self.connection = self.load_channel()
 
     # @detection_timer
     def callback(self, ch, method, properties, body):
         # body : RabbitMQ消息队列中传递的消息
-        # seed heartbeat to keep communication
-        self.keep_alive()
         # define queue message
         data = eval(body.decode("utf-8"))
         frame = pickle.loads(data["frame"])
@@ -126,17 +133,17 @@ class DetectionHandler(object):
                         file = {"file": open(intrusion_result_path, "rb").read()}
                         is_send = send_data(data, file, SERVER_URL_DICT["intrusion_result"])
                         if not is_send:
-                            logger_intrusion.info(f'[INFO] {camera_id} {detection_time} intrusion is_send error.')
+                            logger_handle.info(f'[INFO] {camera_id} {detection_time} intrusion is_send error.')
                     except Exception as e:
-                        logger_intrusion.info(f'[INFO] {camera_id} {detection_time} intrusion is_send error.')
+                        logger_handle.info(f'[INFO] {camera_id} {detection_time} intrusion is_send error.')
 
 
 if __name__ == '__main__':
     # A time to wait rabbitmq running
-    time.sleep(60)
+    # time.sleep(60)
     init_image = cv2.imread(os.path.join(ICV_INSTALL_PATH, "model", "intrusion", "test.jpg"))
-    logger_intrusion = logging_handle(LOGGING_PATH_DICT["intrusion"])
+    logger_handle = logging_handle(LOGGING_PATH_DICT["intrusion"])
     detection_mq = DetectionHandler(INTRUSIONMQ_PARAMS["username"], INTRUSIONMQ_PARAMS["password"],
                                     INTRUSIONMQ_PARAMS["host"],
-                                    INTRUSIONMQ_PARAMS["queue_name"], logger_intrusion, init_image)
+                                    INTRUSIONMQ_PARAMS["queue_name"], logger_handle, init_image)
     detection_mq.detection()
