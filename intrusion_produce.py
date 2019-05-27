36f341272ef230497fa3f33723cd7c99a9e8f902
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-05-27 09:57:09 +0800

    生成者修改

--- intrusion_produce.py
+++ intrusion_produce.py
@@ -4,12 +4,11 @@ import pika
 import multiprocessing as mp
 import pickle
 import time
-import requests
-import json
 import numpy as np
 from util.camera_protocol import *
 from conf.config_setting import *
 from conf.config_function import detection_timer, logging_handle
+from util.manage_floder import manage_dir
 
 
 class ImgHandler(object):
@@ -64,7 +63,7 @@ class ImgHandler(object):
             self.channel, self.connection = self.load_channel()
 
 
-def producer_intrusion(queue, CAMERA_NAME, CAMERA_PWD, camera_ip, camera_id, camera_type):
+def producer_intrusion(queue, CAMERA_NAME, CAMERA_PWD, camera_ip, camera_type):
     '''
     python producer queue to put data
     :param queue_intrusion: python queue name
@@ -86,12 +85,12 @@ def producer_intrusion(queue, CAMERA_NAME, CAMERA_PWD, camera_ip, camera_id, cam
         detection_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         if is_opened:
             # put frame,detection_time,camera_id python queue
-            queue.put((frame, detection_time, camera_id))
+            queue.put((frame, detection_time, camera_ip))
         else:
             # if get frame failed,put None
             queue.put((None, None, None))
             cap = rtsp_camera(CAMERA_NAME, CAMERA_PWD, camera_ip)
-            logger_intrusion.info(f'[INFO] {camera_id} customer_intrusion put queue error.')
+            logger_intrusion.info(f'[INFO] {camera_ip} customer_intrusion put queue error.')
         # keep 1 frame in queue,queue.qsize=2
         queue.get() if queue.qsize() > 1 else None
 
@@ -110,11 +109,11 @@ def customer_intrusion(queue, ori_data, num_camera):
         # seed heartbeat to keep communication
         img_mq.keep_alive()
         # for python queue get data
-        (frame, detection_time, camera_id) = queue.get()
+        (frame, detection_time, camera_ip) = queue.get()
         if isinstance(frame, np.ndarray):
             data = {
                 "frame": pickle.dumps(frame),
-                "camera_id": camera_id,
+                "camera_ip": camera_ip,
                 "ori_data": ori_data,
                 "detection_time": detection_time
             }
@@ -126,37 +125,19 @@ def customer_intrusion(queue, ori_data, num_camera):
 def run():  # mutil camera
     # A time to wait rabbitmq running
     # time.sleep(60)
-    process_intrusion, camera_id_list, camera_ip_list, camera_type_list, ori_data_list = [], [], [], [], []
-    # keep connection mysql to get camera info
-    is_connection = True
-    while is_connection:
-        try:
-            ## API to get face camera info
-            camera_info_data = {
-                "server_ip": ICV_IP
-            }
-            post_camera_data = requests.post(SERVER_URL_DICT["intrusion_camera_info"],
-                                             json=json.dumps(camera_info_data))
-            post_camera_data = json.loads(post_camera_data.text)
-            camera_id_list = post_camera_data["data"]["camera_id_list"]
-            camera_type_list = post_camera_data["data"]["camera_type_list"]
-            ori_data_list = post_camera_data["data"]["ori_data_list"]
-            camera_ip_list = post_camera_data["data"]["camera_ip_list"]
-            is_connection = False
-        except Exception as e:
-            logger_intrusion.info(f'[INFO] query camera info error')
-            time.sleep(120)
-    if camera_id_list:
+    process_intrusion, ori_data_list = [], []
+
+    if ori_data_list:
         # make image dir
-        is_dir = manage_dir(camera_id_list, ICV_IMG_PATH, IMG_NAME_DICT)
+        is_dir = manage_dir(CAMERA_IP_LIST, ICV_IMG_PATH, IMG_NAME_DICT)
         if is_dir:
-            num_camera = len(camera_id_list)
-            queue_list = [mp.Queue(maxsize=2) for _ in camera_id_list]
-            for queue, camera_ip, camera_id, camera_type, ori_data in zip(queue_list, camera_ip_list, camera_id_list,
-                                                                          camera_type_list, ori_data_list):
+            num_camera = len(CAMERA_IP_LIST)
+            queue_list = [mp.Queue(maxsize=2) for _ in CAMERA_IP_LIST]
+            for queue, camera_ip, camera_type, ori_data in zip(queue_list, CAMERA_IP_LIST, CAMERA_TYPE_LIST,
+                                                               ori_data_list):
                 process_intrusion.append(
                     mp.Process(target=producer_intrusion,
-                               args=(queue, CAMERA_NAME, CAMERA_PWD, camera_ip, camera_id, camera_type)))
+                               args=(queue, CAMERA_NAME, CAMERA_PWD, camera_ip, camera_type)))
                 process_intrusion.append(
                     mp.Process(target=customer_intrusion,
                                args=(queue, ori_data, num_camera)))
