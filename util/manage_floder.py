c7fdbd64d06b8ce6df84c75f70f3b76e91eba317
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-05-27 09:56:10 +0800

    文件夹管理

--- util/manage_floder.py
+++ util/manage_floder.py
@@ -5,18 +5,6 @@ import os
 def manage_dir(camera_id_list, icv_img_path, function_name_dict):
     localtime = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     try:
-        # 安全着装结果图片当天文件夹
-        for camera_id in camera_id_list:
-            if not os.path.exists(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["dress_res_name"])):
-                os.makedirs(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["dress_res_name"]))
-        # 安全着装原始图片当天文件夹
-        for camera_id in camera_id_list:
-            if not os.path.exists(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["dress_bp_name"])):
-                os.makedirs(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["dress_bp_name"]))
         # 越界结果图片当天文件夹
         for camera_id in camera_id_list:
             if not os.path.exists(
@@ -29,32 +17,6 @@ def manage_dir(camera_id_list, icv_img_path, function_name_dict):
                     os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["intrusion_bp_name"])):
                 os.makedirs(
                     os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["intrusion_bp_name"]))
-
-        # 车牌识别结果图片当天文件夹
-        for camera_id in camera_id_list:
-            if not os.path.exists(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["car_res_name"])):
-                os.makedirs(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["car_res_name"]))
-        # 车牌识别原始图片当天文件夹
-        for camera_id in camera_id_list:
-            if not os.path.exists(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["car_bp_name"])):
-                os.makedirs(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["car_bp_name"]))
-
-        # 人脸识别背景图片当天文件夹
-        for camera_id in camera_id_list:
-            if not os.path.exists(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["face_back_name"])):
-                os.makedirs(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["face_back_name"]))
-        # 人脸识别人脸图片当天文件夹
-        for camera_id in camera_id_list:
-            if not os.path.exists(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["face_snap_name"])):
-                os.makedirs(
-                    os.path.join(icv_img_path, str(camera_id), localtime, function_name_dict["face_snap_name"]))
         return True
     except Exception as e:
         # logger_script.info(f'[INFO] manage_dir error.')
